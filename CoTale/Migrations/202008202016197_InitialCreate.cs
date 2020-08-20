3930e285540fd94d098665e6ab45fdaa77893f97
taner.durmaz.cs@gmail.com <taner.durmaz.cs@gmail.com>
2020-08-21 00:33:45 +0300

    trying to use non local db

---
+++ CoTale/Migrations/202008202016197_InitialCreate.cs
@@ -0,0 +1,29 @@
+﻿namespace CoTale.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Adventures",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false, maxLength: 118),
+                        ParentId = c.Int(nullable: false),
+                        Price = c.Int(nullable: false),
+                        Order = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Adventures");
+        }
+    }
+}
