ecf81a072e5b8ff7d7e6b8e1749c7af76992972f
taner.durmaz.cs@gmail.com <taner.durmaz.cs@gmail.com>
2020-08-21 21:45:12 +0300

    migration fix ?

---
+++ CoTale/Migrations/202008211844029_trying tog oline.cs
@@ -0,0 +1,16 @@
+﻿namespace CoTale.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class tryingtogoline : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
